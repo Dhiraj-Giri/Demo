55e5ac1f24e4d7d9dc4e9d07f437eb262d7db13f
unknown <dgiri5065@gmail.com>
2022-12-29 22:16:44 +0530

    adding the files related to web25

---
+++ variables/web25/first.js
@@ -0,0 +1,17 @@
+let string = "Masai School";
+
+// while loop- for loop
+// let i = 1;
+// while(i<=5){
+//   if(i%2==0){
+//     console.log(i);
+//   }
+//   i++
+// }
+
+
+for(let i = 1;i <= 5; i++){
+  if(i%2==0){
+    console.log(i);
+  }
+}
---
+++ variables/web25/index.js
@@ -0,0 +1,15 @@
+// let var const
+
+// let a1 = "Masai School";
+// let a2 = "A Transformation in education";
+
+
+// && || !
+
+// to execute the (&&)and operator and print your output all the conditions should be true.
+// to execute the (||)or operator and print your output any one of the conditions should be true.
+
+// condition?statement1:statement2;
+
+
+(1>5)?console.log("Yes"):console.log("No");
